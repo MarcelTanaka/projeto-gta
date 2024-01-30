df356dc5652c481a8305a0575e81f0f54892c506
Marcel Tanaka <158007811+MarcelTanaka@users.noreply.github.com>
2024-01-30 13:14:47 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const botao = document.querySelector(".btn-plataforma");
+console.log(botao);
+
+    const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+    botao.addEventListener("click", () => {
+        elementoPlataformas.classList.toggle("ativo");
+
+} );
+
+
+
+
